402accf3f3318e14e68ef129dde2d8af382ca8f3
Usama Ahmed <usama4745@gmail.com>
2021-11-23 12:42:19 +0500

    added date in proper format

--- src/component/WeatherCard/WeatherCard.js
+++ src/component/WeatherCard/WeatherCard.js
@@ -4,8 +4,8 @@ import './WeatherCard.css';
 function WeatherCard({forecast}) {
     return (
         <div className="card">
+            <div className="forecasttemperature">date: {forecast && new Date(forecast.dt_txt).toLocaleString()}</div>
             <div className="forecasttemperature">temperature: {forecast && forecast.main.temp} &#176;C</div>
-            <div className="forecasttemperature">date: {forecast && new Date(forecast.dt_txt).toDateString()}</div>
             <div className="forecasttemperature">weather condition: {forecast && forecast.weather[0].main}</div>
             <div className="forecasttemperature">heighest and lowest temp: {forecast && forecast.main.temp_min} &#176;C - {forecast && forecast.main.temp_max} &#176;C</div>
         </div>
